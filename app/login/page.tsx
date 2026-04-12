e7db13fb580f1b01545f9a2861ea83e88e9e9ea9
MMarter <mateuszmarter@gmail.com>
2026-04-12 22:46:32 +0200

    login-in progress

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function LoginPage () {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="relative flex flex-1 flex-col justify-center items-center ">
+            <button className="absolute top-4 right-4 px-3 py-1 bg-black text-white rounded-lg dark:bg-white dark:text-black" >
+                Toggle
+            </button>
+            <div className="flex flex-col w-full max-w-sm p-6 bg-white rounded-2xl shadow-md dark:bg-zinc-900">
+                <label htmlFor="email">Email</label>
+                <input type="email" className="dark:bg-zinc-800 rounded-md" />
+            </div>
         </div>
     );
 }
