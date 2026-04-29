5a1075200810f93188e310564053c776d6befed6
MateuszMarter <mateuszmarter@gmail.com>
2026-04-29 14:50:31 +0200

    homepage basics

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,24 @@
+"use client"
+
+import {useRouter} from "next/navigation";
+
 export default function Home () {
+
+    const router = useRouter();
+
+    const logout = () => {
+        localStorage.clear();
+        router.push("/login");
+    }
+
+
     return(
-        <div className="flex">
-            <h1>Home</h1>
-        </div>
+        <main className={`flex items-center justify-center h-screen`}>
+            <div>
+                <button className={`bg-secondary rounded-lg w-full max-w-md py-1 px-2 border border-white-500`} onClick={logout}>
+                    Logout
+                </button>
+            </div>
+        </main>
     );
 }
